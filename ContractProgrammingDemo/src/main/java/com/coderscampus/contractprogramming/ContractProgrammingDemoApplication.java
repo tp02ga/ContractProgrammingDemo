93dd878471ecd8e2d69493af0752367d7d50a483
Trevor Page <trevor@craftycodr.com>
2017-06-19 10:35:50 -0400

    part-1

---
+++ ContractProgrammingDemo/src/main/java/com/coderscampus/contractprogramming/ContractProgrammingDemoApplication.java
@@ -0,0 +1,12 @@
+package com.coderscampus.contractprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractProgrammingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractProgrammingDemoApplication.class, args);
+	}
+}
---
+++ ContractProgrammingDemo/src/main/java/com/coderscampus/contractprogramming/domain/Client.java
@@ -0,0 +1,65 @@
+package com.coderscampus.contractprogramming.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Client
+{
+  private Integer id;
+  private String name;
+  private String email;
+  private String stripeId;
+  private Set<Project> projects = new HashSet<>();
+  
+  @Id @GeneratedValue
+  public Integer getId()
+  {
+    return id;
+  }
+  public void setId(Integer id)
+  {
+    this.id = id;
+  }
+  public String getName()
+  {
+    return name;
+  }
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+  public String getEmail()
+  {
+    return email;
+  }
+  public void setEmail(String email)
+  {
+    this.email = email;
+  }
+  public String getStripeId()
+  {
+    return stripeId;
+  }
+  public void setStripeId(String stripeId)
+  {
+    this.stripeId = stripeId;
+  }
+  
+  @OneToMany(cascade=CascadeType.ALL, mappedBy="client", fetch=FetchType.LAZY)
+  public Set<Project> getProjects()
+  {
+    return projects;
+  }
+  public void setProjects(Set<Project> projects)
+  {
+    this.projects = projects;
+  }
+}
